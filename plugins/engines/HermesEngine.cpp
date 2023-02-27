24b919c08915dca97f7da4a641d713e11d0e37d9
jaime <jcernudagarcia@hawk.iit.edu>
2023-02-27 10:55:45 -0600

    Hermes Engine: First Commit for the engine: 1) Header: based on the implementation of both the ParaView plugin and other examples on the engine layer.    Note: to my knowledge, this implements the step engine instead of the streaming one, would be interesting to look at this option 2) Source: Very hacky, not compiled, literally a copy-paste and reduce from the paraview engine while we figure it out

--- plugins/engines/HermesEngine.cpp
+++ plugins/engines/HermesEngine.cpp
@@ -3,3 +3,135 @@
 //
 
 #include "HermesEngine.h"
+
+#include <adios2/engine/inline/InlineWriter.h>
+
+#include <hermes.h> //TODO: Set this up
+
+#include <iostream>
+#include <sstream>
+
+namespace hermes_plugin
+{
+
+struct HermesEngine::EngineImpl
+{
+    adios2::core::IO *Io;
+    adios2::core::Engine *Writer;
+
+    std::string ScriptFileName;
+    std::string JSONFileName;
+
+    int Rank;
+
+    EngineImpl(adios2::core::ADIOS &adios)
+    {
+        this->Io = &adios.DeclareIO("InlinePluginIO");
+        this->Io->SetEngine("inline");
+        this->Writer = &Io->Open("write", adios2::Mode::Write);
+    }
+
+    void CatalystConfig()
+    {
+        std::cout << "\tCatalyst Library Version: " << CATALYST_VERSION << "\n";
+        std::cout << "\tCatalyst ABI Version: " << CATALYST_ABI_VERSION << "\n";
+
+        conduit_cpp::Node node;
+        catalyst_about(conduit_cpp::c_node(&node));
+        auto implementation = node.has_path("catalyst/implementation")
+                                  ? node["catalyst/implementation"].as_string()
+                                  : std::string("stub");
+        std::cout << "\tImplementation: " << implementation << "\n\n";
+    }
+
+    void CatalystInit()
+    {
+        conduit_cpp::Node node;
+        node["catalyst/scripts/script/filename"].set(this->ScriptFileName);
+
+        // options to set up the fides reader in paraview
+        std::ostringstream address;
+        address << &Io;
+
+        node["catalyst/fides/json_file"].set(this->JSONFileName);
+        node["catalyst/fides/data_source_io/source"].set(std::string("source"));
+        node["catalyst/fides/data_source_io/address"].set(address.str());
+        node["catalyst/fides/data_source_path/source"].set(
+            std::string("source"));
+        node["catalyst/fides/data_source_path/path"].set(
+            std::string("DataReader"));
+        catalyst_initialize(conduit_cpp::c_node(&node));
+
+        if (this->Rank == 0)
+        {
+            this->CatalystConfig();
+        }
+    }
+
+    void CatalystExecute()
+    {
+        auto timestep = this->Writer->CurrentStep();
+        conduit_cpp::Node node;
+        node["catalyst/state/timestep"].set(timestep);
+        // catalyst requires the next one, but when using Fides as the reader
+        // for Catalyst, it will grab the time from the correct adios variable
+        // if it is specified in the data model
+        node["catalyst/state/time"].set(timestep);
+        node["catalyst/channels/fides/type"].set(std::string("fides"));
+
+        // options to set up the fides reader in paraview
+        std::ostringstream address;
+        address << &Io;
+
+        node["catalyst/fides/json_file"].set(this->JSONFileName);
+        node["catalyst/fides/data_source_io/source"].set(std::string("source"));
+        node["catalyst/fides/data_source_io/address"].set(address.str());
+        node["catalyst/fides/data_source_path/source"].set(
+            std::string("source"));
+        node["catalyst/fides/data_source_path/path"].set(
+            std::string("DataReader"));
+
+        // catalyst requires the data node on a channel, but we don't actually
+        // need it when using fides, so just create a dummy object to pass
+        // the validation in catalyst
+        conduit_cpp::Node dummy;
+        dummy["dummy"].set(0);
+        node["catalyst/channels/fides/data"].set(dummy);
+
+        catalyst_execute(conduit_cpp::c_node(&node));
+    }
+};
+
+HermesEngine::HermesEngine(adios2::core::IO &io,
+                           const std::string &name,
+                           adios2::helper::Comm comm)
+: adios2::plugin::PluginEngineInterface(io, name, adios2::Mode::Write,
+                                        comm.Duplicate()),
+  Impl(new EngineImpl(io.m_ADIOS)){
+
+}
+
+HermesEngine::~HermesEngine(){
+
+}
+
+adios2::StepStatus HermesEngine::BeginStep(adios2::StepMode mode,
+                                           const float timeoutSeconds){
+
+}
+
+size_t HermesEngine::CurrentStep() const{
+}
+
+void HermesEngine::EndStep(){
+}
+
+void HermesEngine::PerformPuts() {
+}
+
+
+void HermesEngine::DoClose(const int transportIndex){
+}
+
+} // end namespace hermes_plugin
+
--- plugins/engines/HermesEngine.h
+++ plugins/engines/HermesEngine.h
@@ -5,8 +5,49 @@
 #ifndef ADIOS2_PLUGINS_ENGINES_HERMESENGINE_H_
 #define ADIOS2_PLUGINS_ENGINES_HERMESENGINE_H_
 
-class HermesEngine
+#include "adios2/common/ADIOSMacros.h"
+#include "adios2/common/ADIOSTypes.h"
+#include "adios2/core/IO.h"
+#include "adios2/engine/plugin/PluginEngineInterface.h"
+#include "adios2/helper/adiosComm.h"
+#include "adios2/helper/adiosType.h"
+
+#include <memory>
+
+namespace hermes_plugin{
+
+class HermesEngine : public adios2::plugin::PluginEngineInterface
 {
+
+public:
+    HermesEngine(adios2::core::IO &adios, const std::string &name,
+                        adios2::helper::Comm comm);
+
+    ~HermesEngine() override;
+
+    adios2::StepStatus BeginStep(adios2::StepMode mode,
+                                 const float timeoutSeconds = -1.0) override;
+
+    void EndStep() override;
+
+    size_t CurrentStep() const override;
+
+    void PerformPuts() override;
+
+protected:
+#define declare_type(T)                                                        \
+    void DoPutSync(adios2::core::Variable<T> &, const T *) override;           \
+    void DoPutDeferred(adios2::core::Variable<T> &, const T *) override;
+    ADIOS2_FOREACH_STDTYPE_1ARG(declare_type)
+#undef declare_type
+
+    void DoClose(const int transportIndex = -1) override;
+
+private:
+    struct EngineImpl;
+    std::unique_ptr<EngineImpl> Impl;
 };
+
+} // end namespace hermes_plugin
 
 #endif // ADIOS2_PLUGINS_ENGINES_HERMESENGINE_H_
